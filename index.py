afed64c59e322bc643ca7ade14905df8b1d8a4f1
Дмитрий Смирнов <we-are-legion2013@yandex.ru>
2020-10-13 14:55:21 +0300

    version 1.1

--- index.py
+++ index.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 import sys
 from PyQt5.QtWidgets import (QWidget, QLabel, QLineEdit,
-                             QTextEdit, QGridLayout, QApplication, QPushButton, QFileDialog)
+                             QTextEdit, QGridLayout, QApplication, QPushButton, QFileDialog, QMessageBox)
 import calendar
 import xlsxwriter
 import datetime
@@ -49,34 +49,63 @@ class Example(QWidget):
 
 
     def  buttonClicked(self):
-        options = QFileDialog.Options()
-        options |= QFileDialog.DontUseNativeDialog
-        fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","MS Excel Files (*.xlsx);;All Files (*)", options=options)
-        workbook = xlsxwriter.Workbook(fileName + '.xlsx')
-        worksheet = workbook.add_worksheet()
-        worksheet.set_column('A:A', 100)
-        bold = workbook.add_format({'bold': True})
-        bold.set_font_color('red')
-        i = 0
-        i2 = 13
-        worksheet.write('A' + str(i + 1),str(self.rir[0])+ "." + self.mth + "." + self.yr,bold)
-        i = i+1
-        ti = 0
-        toto = 0
-        time_massive = ['10-11:' ,'11-12:' ,'12-12:30: – обед' ,'12:30-13:30:' ,'13:30-14:30:' ,'14:30-15:30:' ,'15:30-16:00: перерыв','16-17:' ,'17-18:', 'Протоколы:', 'Изучение библиотеки:', 'Разработка программы:']
-        while  i+1 <= len(self.rir):
+        try:
+          options = QFileDialog.Options()
+          options |= QFileDialog.DontUseNativeDialog
+          fileName, _ = QFileDialog.getSaveFileName(self,"QFileDialog.getSaveFileName()","","MS Excel Files (*.xlsx);;All Files (*)", options=options)
+          workbook = xlsxwriter.Workbook(fileName + '.xlsx')
+          worksheet = workbook.add_worksheet()
+          worksheet.set_column('A:A', 30)
+          worksheet.set_column('B:B',120)
+          bold = workbook.add_format({'bold': True})
+          bold.set_font_color('red')
+          cell_format = workbook.add_format({'bold': True, 'bg_color': 'black'})
+          cell_format2 = workbook.add_format({'bold': True, 'bg_color': '#e1f6fa '})
+          i = 0
+          i2 = 13
+          worksheet.write('A' + str(i + 1),str(self.rir[0])+ "." + self.mth + "." + self.yr,bold)
+          worksheet.write('B' + str(i + 1),'',cell_format)
+          i = i+1
+          ti = 0
+          toto = 0
+          merge_format = workbook.add_format({
+            'bold': True,
+            'border': 4,
+            'align': 'left',
+            'valign': 'vcenter',
+            'fg_color': '#e1f6fa',
+          })
+          time_massive = ['10-11:' ,'11-12:' ,'12-12:30: – обед' ,'12:30-13:30:' ,'13:30-14:30:' ,'14:30-15:30:' ,'15:30-16:00: перерыв','16-17:' ,'17-18:', 'Протоколы:', 'Изучение библиотеки:', 'Разработка программы:']
+          while  i+1 <= len(self.rir):
             worksheet.write('A' + str(i + i2 + 1), str(self.rir[i]) + "." + self.mth + "." + self.yr,bold)
+            worksheet.write('B' + str(i + i2 + 1),'', cell_format)
             ti = 0
             while ti + 1 <= len(time_massive):
                  worksheet.write('A' + str(i + i2 + 1 + ti + 1), time_massive[ti])
                  worksheet.write('A' + str(ti+2), time_massive[ti])
+                 worksheet.write('B' + str(i + i2 + 1 + ti + 1), '',merge_format)
+                 worksheet.write('B' + str(ti+2), '',merge_format)
                  ti = ti + 1
                  toto = toto + 1
             toto = toto + 1
             i = i +1
             i2 = i2 + 13
-        i = 0
-        workbook.close()
+          i = 0
+          workbook.close()
+          Qmessa("Готово")
+        except:
+          Qmessa("Произошла ошибка")
+
+class Qmessa(QMessageBox):
+    def __init__(self, a):
+        super().__init__()
+        msg = QMessageBox()
+        msg.setWindowTitle("Info")
+        msg.setText(a)
+        result = msg.setStandardButtons(QMessageBox.Ok)
+        retval = msg.exec_()
+
+
 
 if __name__ == '__main__':
 
